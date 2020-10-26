34e3ec6568d284a5bf3818bb3d8ef2f1136e6c86
hxm <550085798>
2020-10-26 16:19:40 +0800

    Signed-off-by: hxm <550085798>

---
+++ src/main/java/com/hxm/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.hxm.git;
+/**
+ * @Description 
+ * @author hxm Email:550085798@qq.com
+ * @version 1.0
+ * @date 2020年10月26日 下午4:16:24
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Git!");
+	}
+
+}
