8f5d2db75f41b935ebaf95263f0a8722be95212e
hxm <550085798@qq.com>
2020-10-26 18:19:34 +0800

    通过GitHub克隆的项目

---
+++ src/main/java/com/hxm/git/HelloGitHub.java
@@ -0,0 +1,14 @@
+package com.hxm.git;
+/**
+ * @Description 
+ * @author hxm Email:550085798@qq.com
+ * @version 1.0
+ * @date 2020年10月26日 下午6:13:40
+ */
+public class HelloGitHub {
+	public static void main(String[] args) {
+		
+		System.out.println("通过GitHub克隆的项目");
+	}
+
+}
